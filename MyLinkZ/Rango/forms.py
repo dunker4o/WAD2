4939c0240f1b9a79f51bacfc1bdbcd4e928963b9
dunker4o <borko_3g@mail.com>
2016-03-17 14:36:14 +0000

    17/03/2016 Ch8

--- MyLinkZ/Rango/forms.py
+++ MyLinkZ/Rango/forms.py
@@ -32,7 +32,7 @@ class PageForm(forms.ModelForm):
 	class Meta:
         # Provide an association between the ModelForm and a model
 		model = Page
-
+		fields = ('title', 'url', )
         # What fields do we want to include in our form?
         # This way we don't need every field in the model present.
         # Some fields may allow NULL values, so we may not want to include them...
